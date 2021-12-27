dfb5d7e69621f96f618d0a98a16619f45bd335a3
Godofidget2 <82093471+Godofidget2@users.noreply.github.com>
2021-12-27 13:36:26 +0530

    Add files via upload

---
+++ C-89/quiz_game_login.js
@@ -0,0 +1,9 @@
+function addUser()
+{
+    player1_name = document.getElementById("P1_Username").value;
+    player2_name = document.getElementById("P2_Username").value;
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+    
+    window.location="game_page.html";
+}
